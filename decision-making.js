7f8d084eb3495a34ff6371ef34679135dcb63708
Developertaposroy <taposroy616@gmail.com>
2023-04-01 20:07:13 +0600

    making

---
+++ decision-making.js
@@ -0,0 +1,14 @@
+var tapos={ shart:true, color:'white',}
+
+if(tapos  ['color'] =="red"){
+    console.log("Shaird color is red");
+}
+else if(tapos ['color']=="blue"){
+    console.log("Shaird color is blue");
+}
+else if(tapos ['color']=="bluck"){
+    console.log("Shaird color is bluck");
+}
+else{
+    console.log("color is not found");
+}
