570363d6645db1451fc694ca18ab3ebe3418baa8
Developertaposroy <taposroy616@gmail.com>
2023-04-02 17:39:58 +0600

    arrays

---
+++ Es6-function.js
@@ -0,0 +1,85 @@
+// Simple function
+// function MyNumber(){
+// //     var x=10;
+// //     var y=20;
+// //     var z=x+y;
+// //     console.log(z)
+// // }
+// //
+// // MyNumber();
+
+// parameterized functions
+// function number(x,y){
+//     var z=x+y;
+//     console.log(z)
+// }
+// number(100,50)
+
+
+// rest parameters functions
+// function something(...x){
+//     console.log(x[17])
+//
+// }
+// something(1,2,3,4,65,5,5,5,6,6,5,5,8,5,8,5,'A','B','C')
+
+
+// function returns
+//
+// function dataOne(){
+//     return 10;
+// }
+// function dataTwo(){
+//     return dataOne();
+// }
+// function dataThree(){
+//     return 30;
+// }
+//
+// console.log(dataTwo()+dataThree())
+
+
+// anonymous functions মানে... নাম বিহিন(নাম থাকবে না)
+//    var anFu=function(){
+//     return  hello;
+//    }
+//    var anFu=function(){
+//     return  "hello Agin" ;
+//    }
+//
+//    console.log(anFu());
+
+
+// arrow function
+// var myArrow=(...x)=>{
+//     return "hello arrow";
+// }
+// console.log(myArrow())
+// var Myarrow=(...x)=>{
+//    console.log(x)
+// }
+// Myarrow(1,2,5,8,6,9,7,8,9,6,8,9,6,9,6,8,9);
+// var Myarrow=(x)=>{
+//    console.log(x)
+// }
+//
+// Myarrow("hello arrow");
+// var Myarrow=(x)=>{
+//    console.log("hello arrow")
+// }
+// Myarrow();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ arrays.js
@@ -0,0 +1,7 @@
+let MyArray = ["A", 'B', 'C' , 'D']
+
+let  MyArraypro= new Array("A", 'B', 'C' , 'D')
+
+for(let item of MyArraypro)
+
+console.log(item)
