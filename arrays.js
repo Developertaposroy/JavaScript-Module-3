a3137eee907ff8284a19d1d9268944acd7d6b001
Developertaposroy <taposroy616@gmail.com>
2023-04-02 18:00:17 +0600

    arrays

--- arrays.js
+++ arrays.js
@@ -1,7 +1,32 @@
-let MyArray = ["A", 'B', 'C' , 'D']
+// let MyArray = ["A", 'B', 'C' , 'D']
+//
+// let  MyArraypro= new Array("A", 'B', 'C' , 'D')
+//
+// for(let item of MyArraypro)
+//
+// console.log(item)
 
-let  MyArraypro= new Array("A", 'B', 'C' , 'D')
 
-for(let item of MyArraypro)
+// Multidimensional Arrays
 
-console.log(item)
+
+// var Bangladesh= ["Dinajpur", "Rangpur","Thakurgao" , "Rajshahi" ]
+//
+// var India= ["Kolkata", "dilli","bangalur", "haydrabadh"]
+//
+// var china = ["uman", "umanda", "kinkong"]
+//
+// var Rasiya = [Bangladesh,India,china]
+//
+// console.log(Rasiya[0][3]);
+
+
+// array de-structuring
+
+
+var conutry = ["ban", "ind", "paki", "can", "usa"]
+
+
+var [, b,c,,d]=conutry
+
+console.log(b,c,d)
